260b072be5fc67eee9f63fd8f5d13ec2ce3067fe
gsiwatch <gaurishsiwatch@quickenloans.com>
2019-05-08 19:23:48 -0400

    some update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+    private clickMessage: string;
+
+    sendEmail() {
+      this.clickMessage = `Thanks for the interest.
+       Gaurish will reach out to you shortly
+       if he fail to do so, please feel free to email him gaurish.siwatch@quickenloans.com
+       `;
+    }
 }
