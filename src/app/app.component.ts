7278a064785246aa7683de6afb694c0ff926342e
gsiwatch <gaurishsiwatch@quickenloans.com>
2019-05-08 21:08:35 -0400

    final addition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as emailJs from 'emailjs-com';
 
 @Component({
   selector: 'app-root',
@@ -6,12 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    private clickMessage: string;
+    public clickMessage: string;
+    private firstNameValue: string;
+    private lastNameValue: string;
+    showFirstNameError: boolean;
+    showLastNameError: boolean;
 
-    sendEmail() {
-      this.clickMessage = `Thanks for the interest.
-       Gaurish will reach out to you shortly
-       if he fail to do so, please feel free to email him gaurish.siwatch@quickenloans.com
-       `;
+    sendEmail(firstName: string, lastName: string) {
+        this.showFirstNameError = false;
+        this.showLastNameError = false;
+        this.clickMessage = '';
+        if (!firstName) {
+            this.firstNameValue = 'firstName is required';
+            this.showFirstNameError = true;
+        }
+        if (!lastName) {
+            this.lastNameValue = 'lastName is required';
+            this.showLastNameError = true;
+        }
+        if (!firstName || !lastName) {
+            return;
+        }
+
+        const templateParams = {
+            name: `${firstName} ${lastName}`,
+            notes: 'I am interested!!'
+        };
+
+        emailJs.send('gmail', 'template_gNR36YhK', templateParams, 'user_kWg7moTuSFdmWfMgeGdwL')
+            .then((response) => {
+                this.clickMessage = `Successfully sent the email, GaurishSiwatch will respond back soon`;
+                console.log('SUCCESS!', response.status, response.text);
+            }, (error) => {
+                this.clickMessage = `Sending email failed please email to gaurishsiwatch@quickenloans.com`;
+                console.log('FAILED...', error);
+            });
     }
 }
